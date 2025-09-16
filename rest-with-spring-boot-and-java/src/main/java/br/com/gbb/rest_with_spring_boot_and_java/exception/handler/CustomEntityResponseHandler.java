ae0688c31205f7132a4d8fb7eef9c39283a0e1c2
Gláuber Barreto Brito <glauberbbrito@gmail.com>
2025-09-16 08:46:32 -0300

    add 05_ParametersAndExceptionHandler

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/gbb/rest_with_spring_boot_and_java/exception/handler/CustomEntityResponseHandler.java
@@ -0,0 +1,36 @@
+package br.com.gbb.rest_with_spring_boot_and_java.exception.handler;
+
+import br.com.gbb.rest_with_spring_boot_and_java.exception.ExceptionResponse;
+import br.com.gbb.rest_with_spring_boot_and_java.exception.UnsuportedMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@RestController
+public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(),
+                                                           ex.getMessage(),
+                                                           request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsuportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+}
