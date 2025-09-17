db56c4bd64f5ff765af006d806438c245b85f7a2
Gláuber Barreto Brito <glauberbbrito@gmail.com>
2025-09-16 23:00:35 -0300

    ajustes.

--- 06_WorkingWithVerbs/src/main/java/br/com/gbb/rest_with_spring_boot_and_java/services/PersonServices.java
+++ 06_WorkingWithVerbs/src/main/java/br/com/gbb/rest_with_spring_boot_and_java/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.gbb.rest_with_spring_boot_and_java.services;
 import br.com.gbb.rest_with_spring_boot_and_java.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -10,7 +12,17 @@ import java.util.logging.Logger;
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll(){
+        logger.info("Find All People!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0 ; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
 
     public Person findById(Long id){
         logger.info("Finding person by id " + id);
@@ -20,6 +32,17 @@ public class PersonServices {
         person.setFirstName("Gláuber");
         person.setLastName("Barreto Brito");
         person.setAddress("Patos - Paraíba - Brasil");
+        person.setGender("Male");
+
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Some Address in Brasil");
         person.setGender("Male");
 
         return person;
