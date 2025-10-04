4b6e46d2a910a032ec9cf5701fdeb802e0c0a716
Gláuber Barreto Brito <glauberbbrito@gmail.com>
2025-10-04 08:17:20 -0300

    feat: Initialize step 12, custom response api

---
+++ 12_CustomSerialization/src/main/java/br/com/gbb/rest_with_spring_boot_and_java/data/dto/v1/PersonDTO.java
@@ -0,0 +1,74 @@
+package br.com.gbb.rest_with_spring_boot_and_java.data.dto.v1;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@JsonPropertyOrder({"id", "first_name", "last_name", "address"})
+public class PersonDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+    private String address;
+
+    @JsonIgnore
+    private String gender;
+
+    public PersonDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() { return address; }
+
+    public void setAddress(String address) { this.address = address; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTO person)) return false;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getGender(), person.getGender()) && Objects.equals(getAddress(), person.getAddress());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getGender(), getAddress());
+    }
+}
---
+++ 12_CustomSerialization/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.person
+(id, address, first_name, gender, last_name)
+VALUES(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna');
+INSERT INTO public.person
+(id, address, first_name, gender, last_name)
+VALUES(2, 'Patos - Paraíba - Brasil 123', 'Miguel', 'Male', 'R Brito');
+INSERT INTO public.person
+(id, address, first_name, gender, last_name)
+VALUES(7, 'Patos - Paraíba - Brasil 123', 'Gláuber', 'Male', 'Barreto Brito');
+INSERT INTO public.person
+(id, address, first_name, gender, last_name)
+VALUES(10, 'Patos - Paraíba - Brasil', 'Mateus', 'Male', 'R Brito');
+INSERT INTO public.person
+(id, address, first_name, gender, last_name)
+VALUES(11, 'Patos - Paraíba - Brasil', 'Mateus', 'Male', 'R Brito');
+INSERT INTO public.person
+(id, address, first_name, gender, last_name)
+VALUES(14, 'Patos - Paraíba - Brasil', 'Mateus', 'Male', 'R Brito');
