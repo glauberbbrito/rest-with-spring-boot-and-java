85dcbc1a6dce78d288cc5dc3d01bcd2243d78e43
Gláuber Barreto Brito <glauberbbrito@gmail.com>
2025-10-04 22:18:38 -0300

    feat: Initialize step 14, add HATEOAS

---
+++ 14_HATEOAS/src/main/java/br/com/gbb/rest_with_spring_boot_and_java/controllres/PersonalController.java
@@ -0,0 +1,58 @@
+package br.com.gbb.rest_with_spring_boot_and_java.controllres;
+
+import br.com.gbb.rest_with_spring_boot_and_java.data.dto.v1.PersonDTO;
+import br.com.gbb.rest_with_spring_boot_and_java.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonalController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+                            MediaType.APPLICATION_XML_VALUE,
+                            MediaType.APPLICATION_YAML_VALUE})
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE,
+                                               MediaType.APPLICATION_XML_VALUE,
+                                               MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping(value = "/create", consumes = {MediaType.APPLICATION_JSON_VALUE,
+                                                  MediaType.APPLICATION_XML_VALUE,
+                                                  MediaType.APPLICATION_YAML_VALUE},
+                                      produces = {MediaType.APPLICATION_JSON_VALUE,
+                                                  MediaType.APPLICATION_XML_VALUE,
+                                                  MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return service.create(person);
+    }
+
+    @PutMapping(value = "/update", consumes = {MediaType.APPLICATION_JSON_VALUE,
+                                                 MediaType.APPLICATION_XML_VALUE,
+                                                 MediaType.APPLICATION_YAML_VALUE},
+                                     produces = {MediaType.APPLICATION_JSON_VALUE,
+                                                 MediaType.APPLICATION_XML_VALUE,
+                                                 MediaType.APPLICATION_YAML_VALUE})
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
