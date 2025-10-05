baf89b692284861d417deb2f606cf58c74dce9c2
Gláuber Barreto Brito <glauberbbrito@gmail.com>
2025-10-04 22:13:58 -0300

    feat: Finish step 13, add Content Negotiation

--- 13_ContentNegotiation/src/main/java/br/com/gbb/rest_with_spring_boot_and_java/controllres/PersonalController.java
+++ 13_ContentNegotiation/src/main/java/br/com/gbb/rest_with_spring_boot_and_java/controllres/PersonalController.java
@@ -16,22 +16,36 @@ public class PersonalController {
     @Autowired
     private PersonServices service;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+                            MediaType.APPLICATION_XML_VALUE,
+                            MediaType.APPLICATION_YAML_VALUE})
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE,
+                                               MediaType.APPLICATION_XML_VALUE,
+                                               MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/create", consumes = {MediaType.APPLICATION_JSON_VALUE,
+                                                  MediaType.APPLICATION_XML_VALUE,
+                                                  MediaType.APPLICATION_YAML_VALUE},
+                                      produces = {MediaType.APPLICATION_JSON_VALUE,
+                                                  MediaType.APPLICATION_XML_VALUE,
+                                                  MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
-    @PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/update", consumes = {MediaType.APPLICATION_JSON_VALUE,
+                                                 MediaType.APPLICATION_XML_VALUE,
+                                                 MediaType.APPLICATION_YAML_VALUE},
+                                     produces = {MediaType.APPLICATION_JSON_VALUE,
+                                                 MediaType.APPLICATION_XML_VALUE,
+                                                 MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
